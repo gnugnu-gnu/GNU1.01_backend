c00e7a4b4f1a56f65410379f3feae4a03915f78f
gnugnu-gnu <qiguai0805@gmail.com>
2025-04-25 00:10:46 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const db = require('./src/config/db');
+
+const bedsRouter = require('./routes/beds');
+const cropsRouter = require('./routes/crops');
+const todosRouter = require('./routes/todos');
+const cropTypesRouter = require('./routes/cropTypes');
+
+app.use(cors());
+app.use(express.json());
+app.use('/beds', bedsRouter);
+app.use('/crops', cropsRouter);
+app.use('/todos', todosRouter);
+app.use('/crop-types', cropTypesRouter);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
