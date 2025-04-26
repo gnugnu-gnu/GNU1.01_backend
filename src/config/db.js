ada32a226c761eb515cfd221041a829a43ecd8bb
gnugnu-gnu <qiguai0805@gmail.com>
2025-04-26 20:24:11 +0900

    Add files via upload

---
+++ src/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log("MongoDB 연결 성공"))
+  .catch(err => console.error("MongoDB 연결 오류:", err.message));
+
+module.exports = mongoose;
