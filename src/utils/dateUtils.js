e32b82e8b364c564f7abf7a47ba629817261a529
gnugnu-gnu <qiguai0805@gmail.com>
2025-04-30 21:08:54 +0900

    Delete src directory

--- src/utils/dateUtils.js
+++
@@ -1,7 +0,0 @@
-function getNextDate(lastDate, cycle) {
-  const date = new Date(lastDate);
-  date.setDate(date.getDate() + cycle);
-  return date.toISOString().split('T')[0];
-}
-
-module.exports = { getNextDate };
